9a1d12e0de936200401b169de123158bb9aae2b5
TahaaFatima <55625397+TahaaFatima@users.noreply.github.com>
2022-10-03 12:20:38 +0500

    Update settings.py

--- nft/settings.py
+++ nft/settings.py
@@ -128,9 +128,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 STATIC_ROOT = os.path.join(BASE_DIR, "marketplace/static")
 
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'marketplace/static/'),
-)
+# STATICFILES_DIRS = (
+# os.path.join(BASE_DIR, 'marketplace/static/'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
